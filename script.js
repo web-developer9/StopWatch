1746b69b14730d32137ec39855334c70e3d17831
Navneet2web <navneet2web@gmail.com>
2023-07-27 20:50:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const watching = document.getElementById("watch");
+const minus = document.getElementById("minus");
+const reset = document.getElementById("reset");
+const hourswatch = document.getElementById("hours");
+const miliseconds = document.getElementById("milisec");
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let milisec = 0;
+sub.addEventListener("click", function () {
+  function con() {
+    milisec += 1;
+    if (milisec === 100) {
+      seconds += 1;
+      milisec = 0;
+    }
+    if (seconds === 60) {
+      minutes = minutes + 1;
+      seconds = 0;
+    }
+    if (minutes === 60) {
+      hours += 1;
+
+      minutes = 0;
+    }
+    seconds < 10
+      ? (watching.textContent = "0" + seconds)
+      : (watching.textContent = seconds);
+    minutes < 10
+      ? (minus.textContent = "0" + minutes)
+      : (minus.textContent = minutes);
+    hours < 10
+      ? (hourswatch.textContent = "0" + hours)
+      : (hourswatch.textContent = hours);
+    miliseconds.textContent = milisec;
+  }
+
+  myInterval = setInterval(con, 10);
+});
+
+sto.addEventListener("click", function () {
+  clearInterval(myInterval);
+});
+
+reset.addEventListener("click", function () {
+  seconds = 0;
+  minutes = 0;
+  milisec = 0;
+  minus.textContent = minutes;
+  watching.textContent = seconds;
+  hourswatch.textContent = hours;
+  miliseconds.textContent = milisec;
+  clearInterval(myInterval);
+});
